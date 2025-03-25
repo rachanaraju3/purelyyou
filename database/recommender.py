670fd92cb46001bcbc71cc6cad0e9b598d939a08
Soumily Maji <majisoumily14@gmail.com>
2025-03-25 09:54:18 -0700

    Added filtering kind of

---
+++ database/recommender.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+full_df = pd.read_csv('skincare_products.csv')
+
+cleanser_df = full_df[full_df['Category'] == 'cleansers']
+toner_df = full_df[full_df['Category'] == 'toners']
+serum_df = full_df[full_df['Category'] == 'serums']
+moist_df = full_df[full_df['Category'] == 'moisturizers']
+
+def filter_by_feature(df, feature):
+    return df[df['Features'].apply(lambda x: feature in x)]
+
+def filter_out_features(df, features_to_exclude):
+    return df[df['Features'].apply(lambda x: not any(feature in x for feature in features_to_exclude))]
+
+
+filter_out_clean = filter_out_features(cleanser_df, exclude)
+cleanser_filtered = filter_by_feature(filter_out_clean, include)
+
+filter_out_toner = filter_out_features(toner_df, exclude)
+toner_filtered = filter_by_feature(filter_out_toner, include)
+
+filter_out_serum = filter_out_features(serum_df, exclude)
+serum_filtered = filter_by_feature(filter_out_serum, include)
+
+filter_out_cream = filter_out_features(moist_df, exclude)
+moist_filtered = filter_by_feature(filter_out_cream, include)
